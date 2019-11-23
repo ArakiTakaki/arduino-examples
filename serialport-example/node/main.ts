6427dc54f0be8895567834e3abd2254d0693ce65
ArakiTakaki <araki_takaki@yahoo.co.jp>
2019-11-23 16:59:32 +0900

    ok

--- serialport-example/node/main.ts
+++ serialport-example/node/main.ts
@@ -1,7 +1,11 @@
 import * as SerialPort  from 'serialport';
 // $ ls /dev/ | grep cu.usb | pbcopy
 const portName = '/dev/cu.usbmodem143401'; // Mac環境
-const arduino = new SerialPort(portName, (error) => {
+
+const arduino = new SerialPort(portName, {
+  baudRate: 9600 / 2,
+}, (error) => {
+  if (error == null) return;
   console.error('通信できませんでした');
   console.error(error);
   throw error;
